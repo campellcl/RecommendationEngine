2ef25e81208ce50874488c0ab8b2b263cc5785e4
kilukmt <kilukmt@appstate.edu>
2016-04-13 10:35:55 -0400

    Added files via upload

--- rating.py
+++ rating.py
@@ -9,10 +9,14 @@
 import numpy as np
 import time
 
-#load data, get user list, get mean ratings
+#load data, get number of movies, get user list, get mean ratings
 data = np.load('train.npy')
+numMovies = len(data)
 userList = data[:,0]
 meanRatings = np.mean(data[:,2])
+
+#meanMovie = np.mean(data[:,1])
+#meanUser = np.mean(data[:,0])
 
 #subtract mean ratings from actual ratings
 data[:,2] = data[:,2] - meanRatings
@@ -21,4 +25,5 @@ data[:,2] = data[:,2] - meanRatings
 for j in range numMovies
     a[j] = np.mean(data[data[:,1] == j,2])
     data[data[:,1] = j,2] -= a[j]
-     
+
+    
